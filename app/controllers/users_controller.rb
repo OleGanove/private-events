72dd150d5e4240622bb714f7c28f9bfc3cd15a94
Ngoc Truong <truong@tutanota.de>
2015-07-17 23:53:38 +0200

    Create layout and authentication system

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+      sign_in @user
+      flash[:error] = "Welcome to the events app"
+  	  redirect_to @user
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def destroy
+  end	
+
+  private 
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
